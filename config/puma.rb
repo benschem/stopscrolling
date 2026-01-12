101037cb254f574fcc3c842434254fe80111f70c
Ben Schembri <benschembri@gmail.com>
2026-01-13 00:25:02 +1100

    feat: update puma config for production

--- config/puma.rb
+++ config/puma.rb
@@ -26,18 +26,46 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch('RAILS_MAX_THREADS', 3)
+threads_count = ENV.fetch('RAILS_MAX_THREADS', 2)
 threads threads_count, threads_count
 
+# Worker count - start with 1 for SQLite, can increase if needed
+workers 1
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch('PORT', 3000)
+# port ENV.fetch('PORT', 3000)
+
+# Use Unix socket instead of port
+# TO DO: Don't hardcode this
+if ENV.fetch('RAILS_ENV', 'production').downcase == 'production'
+  bind 'unix:///var/www/stopscrolling-production/shared/tmp/sockets/puma.sock'
+elsif ENV.fetch('RAILS_ENV', 'staging').downcase == 'staging'
+  bind 'unix:///var/www/stopscrolling-staging/shared/tmp/sockets/puma.sock'
+else
+  port ENV.fetch('PORT', 3000)
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV['SOLID_QUEUE_IN_PUMA']
+plugin :solid_queue
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV['PIDFILE'] if ENV['PIDFILE']
+if ENV.fetch('RAILS_ENV', 'production').downcase == 'production'
+  # Specify the PID file. Defaults to tmp/pids/server.pid in development.
+  # In other environments, only set the PID file if requested.
+  # pidfile ENV['PIDFILE'] if ENV['PIDFILE']
+  # TO DO: Don't hardcode these
+  pidfile '/var/www/stopscrolling-production/shared/tmp/pids/puma.pid'
+  state_path '/var/www/stopscrolling-production/shared/tmp/pids/puma.state'
+
+  # Logging
+  # TO DO: Don't hardcode these
+  stdout_redirect '/var/www/stopscrolling-production/shared/log/puma.stdout.log',
+                  '/var/www/stopscrolling-production/shared/log/puma.stderr.log', true
+end
